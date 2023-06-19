4b35927fb532396e589c0bd8ce67d3eafac4e09f
Heri Kurniawan <95902725+herikurn1@users.noreply.github.com>
2023-06-19 09:19:35 +0700

    Add files via upload

---
+++ resources/views/history.blade.php
@@ -0,0 +1,76 @@
+<head>
+
+  
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css" rel="stylesheet" type="text/css">
+  <link href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css">
+
+  
+  <link href="https://code.jquery.com/jquery-3.5.1.js" rel="stylesheet">
+  <link href="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js" rel="stylesheet"/>
+  <link href="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap4.min.js" rel="stylesheet" />
+  </head>
+<table id="example" class="table table-striped table-bordered" style="width:100%">
+  <thead>
+      <tr>
+          <th>User ID</th>
+          <th>Name</th>
+          <th>Date</th>
+          <th>Time</th>
+          <th>Status</th>
+      </tr>
+  </thead>
+  <tbody>
+      <tr>
+        <td>0346789</td>
+        <td>Yuan Gito Saputra</td>
+        <td>14/03/2023</td>
+        <td>17:30 PM</td>
+        <td>Oud-Turnhout</td>
+        <td>Sent</td>
+      </tr>
+      <tr>
+        <td>0346799</td>
+        <td>Heri Kurniawan</td>
+        <td>14/04/2023</td>
+        <td>17:30 PM</td>
+        <td>Sinaai-Waas</td>
+        <td>Sent</td>
+      </tr>
+      <tr>
+        <td>0346899</td>
+        <td>Hasna Tania</td>
+        <td>14/05/2023</td>
+        <td>17:30 PM</td>
+        <td>Baileux</td>
+        <td>Sent</td>
+      </tr>
+      <tr>
+        <td>0346999</td>
+        <td>Jon Ken</td>
+        <td>14/05/2023</td>
+        <td>17:30 PM</td>
+        <td>Baileux</td>
+        <td>Not Sent</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>0347999</td>
+        <td>Lola Loli</td>
+        <td>14/05/2023</td>
+        <td>17:30 PM</td>
+        <td>Feldkirchen in Kärnten</td>
+        <td>Not sent</td>
+      </tr>
+      <tr>
+        <td>0348999</td>
+        <td>Garet Eston</td>
+        <td>14/05/2023</td>
+        <td>17:30 PM</td>
+        <td>Gloucester</td>
+        <td>Not Sent</td>
+      </tr>
+      <tr>
+          
+  </tbody>
+  
+</table>
